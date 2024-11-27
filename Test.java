18411bcd584727b262f61bef665b661754084156
Chandana-5678 <nuthalapatichandhana@gmail.com>
2024-11-27 11:36:56 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,8 +1,7 @@
 class Test
     {
-        public static void main (string[] args)
+        public static void main(String args[])
         {
-        System.out.println("hello world"); 
-
-    }
-};
+            System.out.println("hello world");
+        }
+    };
